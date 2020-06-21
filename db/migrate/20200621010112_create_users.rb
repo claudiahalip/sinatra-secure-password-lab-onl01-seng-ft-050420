7aaec438020dee349392914a9dcd4eb33fd9aeb8
claudia v <claudia4ro@yahoo.com>
2020-06-21 01:02:42 +0000

    Automatically backed up by Learn

--- db/migrate/20200621010112_create_users.rb
+++ db/migrate/20200621010112_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_tabe :uses do |t|
       t.string :username
-      t.string :password_
+      t.string :password_digest
     end 
   end
 end
